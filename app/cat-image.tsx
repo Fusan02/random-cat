60ee0dc0341d666513a4cc6005b29477c65786da
Fusan02 <saitoufutoshi0909@gmail.com>
2025-08-06 17:46:56 +0900

    first commit

---
+++ app/cat-image.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+import styles from "./page.module.css";
+
+type CatImageProps = {
+  url: string;
+};
+
+const CatImage = ({ url }: CatImageProps) => {
+  // useStateを使って状態を管理
+  const [imageUrl, setImageUrl] = useState(url);
+
+  // 画像を取得する関数を定義
+  const refreshImage = async () => {
+    setImageUrl(""); // 初期化
+    const image = await fetchImage();
+    setImageUrl(image.url);
+  };
+
+  return (
+    <div className={styles.page}>
+      <button onClick={refreshImage} className={styles.button}>
+        他のニャンコも見る
+      </button>
+      <div className={styles.frame}>
+        {imageUrl && <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
+};
+
+export default CatImage;
